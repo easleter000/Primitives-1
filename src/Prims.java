9c3d028449bc7bdb234462946568deb9cacd52af
Terry Easley <easleter000@warren.k12.in.us>
2024-08-01 13:28:06 -0400

    U1L1.2

---
+++ src/Prims.java
@@ -0,0 +1,39 @@
+public class Prims {
+
+    public static void main(String[] args) {
+        int myInt = 7;
+        System.out.println(myInt);
+        short myShort = 8;
+        System.out.println(myShort);
+        byte myByte = 9;
+        System.out.println(myByte);
+        long myLong = 6837548635364L;
+
+        String myString = "Hello World";
+
+        char myChar = 'a';
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+
+        Integer myInt2 = 7;
+
+        System.out.println(myInt);
+        Short myShort2 = 8;
+        System.out.println(myShort);
+        Byte myByte2 = 9;
+        System.out.println(myByte);
+        Long myLong2 = 6837548635364L;
+        System.out.println(myLong);
+
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+    }
+
+}
